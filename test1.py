8ae8900164b10c43c1e04c5ac39578948321fab6
Nik777W <paderov.e@gmail.com>
2024-11-16 12:31:18 -0300

    test7

--- test1.py
+++ test1.py
@@ -1,3 +1,11 @@
-a = 45
-b = 55
-print(a + b)
+import sys
+
+summa = map(lambda x, y, z: x + y + z, [1, 2], [3, 4], [5, 6])
+powers = map(pow, [2, 3, 4], [4, 2, 3])
+a = sys.getsizeof
+
+
+
+
+print(*summa)
+print(*powers)
